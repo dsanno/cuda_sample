849e01737605486702fec2e8bb32e48efd7799f7
Daiki.Sanno <dsanno@adachi.ne.jp>
2015-09-14 23:06:51 +0900

    Fix sudoku_cpu

--- sudoku_cpu/main.c
+++ sudoku_cpu/main.c
@@ -105,19 +105,22 @@ load(char *in_file_path, int *out_number) {
 }
 
 static int
-solve(int *in_static_number, int in_max_count, int *out_result) {
+solve(int *in_static_number, int in_max_count, int *out_result)
+{
 	int row_flag[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 	int col_flag[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 	int box_flag[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	int empty[81];
 	int number[81];
 	int found = 0;
 	int pos;
 	int flag;
-#if 1
-	int count = 0;
-	char nn[16384][50];
-#endif
+	int index;
+	int empty_num;
+	int i;
+	int *result = out_result;
 
+	index = 0;
 	for (pos = 0; pos < 81; pos++) {
 		number[pos] = in_static_number[pos];
 		if (in_static_number[pos] > 0) {
@@ -126,82 +129,55 @@ solve(int *in_static_number, int in_max_count, int *out_result) {
 			col_flag[col[pos]] |= flag;
 			box_flag[box[pos]] |= flag;
 		}
+		else {
+			empty[index] = pos;
+			index++;
+		}
 	}
-	pos = 0;
-	while (pos < 81 && in_static_number[pos] > 0) {
-		pos++;
-	}
+	empty_num = index;
+	index = 0;
 	while (1) {
-		if (pos < 0) {
-			break;
-		}
-#if 1 // test
-		if (pos >= 50) {
-			if (found < 16384) {
-				for (int i = 0; i < 50; i++) {
-					nn[found][i] = number[i];
+		pos = empty[index];
+		for (number[pos]++; number[pos] < 10; number[pos]++) {
+			flag = 1 << number[pos];
+			if ((row_flag[row[pos]] & flag) != 0 || (col_flag[col[pos]] & flag) != 0 || (box_flag[box[pos]] & flag) != 0) {
+				continue;
+			}
+			if (index >= empty_num - 1) {
+				for (i = 0; i < 81; i++) {
+					result[i] = number[i];
+				}
+				found++;
+				result += 81;
+				if (found >= in_max_count) {
+					break;
 				}
 			}
-			found++;
-			pos--;
-			while (pos >= 0 && in_static_number[pos] > 0) {
-				pos--;
-			}
-			if (pos >= 0) {
-				flag = 1 << number[pos];
-				row_flag[row[pos]] &= ~flag;
-				col_flag[col[pos]] &= ~flag;
-				box_flag[box[pos]] &= ~flag;
-			}
-			continue;
-		}
-#endif
-		if (pos >= 81) {
-			if (found == 0) {
-				for (int i = 0; i < 81; i++) {
-					out_result[i] = number[i];
-				}
-			}
-			found++;
-			if (found >= in_max_count) {
+			else {
 				break;
 			}
-			pos--;
-			while (pos >= 0 && in_static_number[pos] > 0) {
-				pos--;
-			}
-			if (pos >= 0) {
-				flag = 1 << number[pos];
-				row_flag[row[pos]] &= ~flag;
-				col_flag[col[pos]] &= ~flag;
-				box_flag[box[pos]] &= ~flag;
-			}
-			continue;
 		}
-		do {
-			number[pos]++;
+		if (found >= in_max_count) {
+			break;
+		}
+		if (number[pos] < 10) {
 			flag = 1 << number[pos];
-		} while (number[pos] <= 9 && ((row_flag[row[pos]] & flag) != 0 || (col_flag[col[pos]] & flag) != 0 || (box_flag[box[pos]] & flag) != 0));
-		if (number[pos] > 9) {
-			number[pos] = 0;
-			pos--;
-			while (pos >= 0 && in_static_number[pos] > 0) {
-				pos--;
-			}
-			if (pos >= 0) {
-				flag = 1 << number[pos];
-				row_flag[row[pos]] &= ~flag;
-				col_flag[col[pos]] &= ~flag;
-				box_flag[box[pos]] &= ~flag;
-			}
-		} else {
 			row_flag[row[pos]] |= flag;
 			col_flag[col[pos]] |= flag;
 			box_flag[box[pos]] |= flag;
-			pos++;
-			while (pos < 81 && in_static_number[pos] > 0) {
-				pos++;
+			index++;
+		}
+		else {
+			index--;
+			if (index < 0) {
+				break;
 			}
+			number[pos] = 0;
+			pos = empty[index];
+			flag = 1 << number[pos];
+			row_flag[row[pos]] &= ~flag;
+			col_flag[col[pos]] &= ~flag;
+			box_flag[box[pos]] &= ~flag;
 		}
 	}
 	return found;
